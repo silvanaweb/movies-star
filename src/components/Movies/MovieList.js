4fd55cb05eced80cb07c64d9fc819cb873bcac90
Silvana Donato <silvana.donato@moixa.com>
2019-04-17 10:35:35 +0100

    add  animation to movie list

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -2,19 +2,18 @@ import React from "react";
 import "./MovieList.css";
 import { MovieListItem } from "./MovieListItem";
 
-const MovieList = ({ movies }) => {
-  return (
-    <div className="MovieList__list">
-      {movies.map(({ title, genres, poster_path, id }) => (
-        <MovieListItem
-          key={`${title}${id}`}
-          title={title}
-          genres={genres.join(", ")}
-          image={poster_path}
-        />
-      ))}
-    </div>
-  );
-};
+const MovieList = ({ movies }) => (
+  <div className="MovieList__list">
+    {movies.map(({ title, genres, poster_path, id }, idx) => (
+      <MovieListItem
+        key={`${title}${id}`}
+        title={title}
+        genres={genres.join(", ")}
+        image={poster_path}
+        index={idx}
+      />
+    ))}
+  </div>
+);
 
 export { MovieList };
--- src/components/Movies/MovieListItem.js
+++ src/components/Movies/MovieListItem.js
@@ -4,16 +4,26 @@ const mkImagePath = name => {
   return `https://image.tmdb.org/t/p/w200/${name}`;
 };
 
-const MovieListItem = ({ genres, title, image }) => (
-  <div className="MovieList__list__item">
-    <div>
-      <img src={mkImagePath(image)} alt={title} />
+const MovieListItem = ({ genres, title, image, index }) => {
+  const [show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 80 * index);
+  }, []);
+
+  return (
+    <div className="MovieList__list__item" style={{ opacity: show ? 1 : 0 }}>
+      <div>
+        <img src={mkImagePath(image)} alt={title} />
+      </div>
+      <div className="MovieList__list__item__content">
+        <h3>{title}</h3>
+        <p>{genres}</p>
+      </div>
     </div>
-    <div className="MovieList__list__item__content">
-      <h3>{title}</h3>
-      <p>{genres}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export { MovieListItem };
