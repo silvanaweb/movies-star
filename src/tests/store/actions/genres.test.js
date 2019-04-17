d0aed1d34ac2c4ad73e4142b89a1e8ec76eaa200
Silvana Donato <silvana.donato@moixa.com>
2019-04-17 16:11:03 +0100

    testing setup

--- src/tests/store/actions/genres.test.js
+++ src/tests/store/actions/genres.test.js
@@ -1,5 +1,10 @@
-import { setGenres } from "../../../store/actions/genres";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { setGenres, startSetGenres } from "../../../store/actions/genres";
 import { genres } from "../../fixtures/genres";
+const createMockStore = configureMockStore([thunk]);
+
+// ideally we should a test DB to which we are connecting for testing purposes
 
 test("should generate set s genres correctly", () => {
   const action = setGenres(genres);
@@ -9,4 +14,14 @@ test("should generate set s genres correctly", () => {
   });
 });
 
-
+test("should fetch the expenses from firebase", done => {
+  const store = createMockStore();
+  store.dispatch(startSetGenres()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: "SET_GENRES",
+      expenses
+    });
+    done();
+  });
+});
