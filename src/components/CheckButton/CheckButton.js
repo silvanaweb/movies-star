5473e4e47f17b4127eb1c0d03fd62a612121e434
silvanaweb <silvanasono@yahoo.it>
2019-04-18 19:22:26 +0100

    improving filters style

--- src/components/CheckButton/CheckButton.js
+++ src/components/CheckButton/CheckButton.js
@@ -1,25 +1,16 @@
-import React from "react";
-import "./CheckButton.css";
+import React from 'react';
+import './CheckButton.css';
 
 // check button is used for the genre filtering
-export const CheckButton = ({
-  checked,
-  className = "",
-  name,
-  onChange,
-  title
-}) => {
-  const onHandleChange = e => {
-    e.stopPropagation();
-    onChange({ name, checked: !checked });
-  };
-  const ButtonCheckedClass = checked ? "CheckButton--checked" : "";
-  return (
-    <button
-      className={`CheckButton ${ButtonCheckedClass} ${className}`}
-      onClick={onHandleChange}
-    >
-      {title}
-    </button>
-  );
+export const CheckButton = ({ checked, className = '', name, onChange, title }) => {
+	const onHandleChange = e => {
+		e.stopPropagation();
+		onChange({ name, checked: !checked });
+	};
+	const ButtonCheckedClass = checked ? 'CheckButton--checked' : '';
+	return (
+		<button className={`CheckButton ${ButtonCheckedClass} ${className}`} onClick={onHandleChange}>
+			{title}
+		</button>
+	);
 };
