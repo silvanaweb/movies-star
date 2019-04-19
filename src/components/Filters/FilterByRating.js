43f9b7480a6954d2a8da39ab9fe7cb93ac399b98
silvanaweb <silvanasono@yahoo.it>
2019-04-19 08:23:19 +0100

    refactor range slider

--- src/components/Filters/FilterByRating.js
+++ src/components/Filters/FilterByRating.js
@@ -5,35 +5,36 @@ import { RangeSlider } from "../RangeSlider/RangeSlider";
 
 // the Rating Filter is an input range
 const FilterByRating = ({ className, setSelectedRating, rating }) => {
-  const onSelectedRatingChange = rating => {
-    // dispatch to filters
-    setSelectedRating(rating);
-  };
-  return (
-    <div className={className}>
-      <RangeSlider
-        defaultValue={rating}
-        min={1}
-        max={10}
-        step={0.5}
-        onValueChange={onSelectedRatingChange}
-      />
-    </div>
-  );
+	const onSelectedRatingChange = rating => {
+		// dispatch to filters
+		setSelectedRating(rating);
+	};
+	return (
+		<div className={className}>
+			<RangeSlider
+				defaultValue={rating}
+				labels={{ tooltip: "rating from" }}
+				min={1}
+				max={10}
+				step={0.5}
+				onValueChange={onSelectedRatingChange}
+			/>
+		</div>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    rating: state.filters.rating
-  };
+	return {
+		rating: state.filters.rating,
+	};
 };
 const mapDispatchToProps = dispatch => ({
-  setSelectedRating: rating => dispatch(setSelectedRating(rating))
+	setSelectedRating: rating => dispatch(setSelectedRating(rating)),
 });
 
 export { FilterByRating };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(FilterByRating);
--- src/components/RangeSlider/RangeSlider.js
+++ src/components/RangeSlider/RangeSlider.js
@@ -2,97 +2,86 @@ import React, { useEffect, useState } from "react";
 import "./RangeSlider.css";
 
 const moods = {
-  NEUTRAL: "neutral",
-  BAD: "bad",
-  GOOD: "good",
-  BLISSFUL: "blissful",
-  BADBAD: "very-bad"
+	NEUTRAL: "neutral",
+	BAD: "bad",
+	GOOD: "good",
+	BLISSFUL: "blissful",
+	BADBAD: "very-bad",
 };
 
 const RangeSlider = ({
-  defaultValue = 5,
-  min = 1,
-  max = 10,
-  step = 1,
-  onValueChange = () => undefined
+	defaultValue = 5,
+	labels = { tooltip: "" },
+	min = 1,
+	max = 10,
+	step = 1,
+	onValueChange = () => undefined,
 }) => {
-  const [value, setValue] = useState(`${defaultValue}`);
-  const [mood, setMood] = useState(moods.NEUTRAL);
-  const [tooltipPos, setTooltipPos] = useState(-1);
-  const [showTooltip, setShowtooltip] = useState(false);
-  const badMoodLimit = max * 0.4;
-  const gooMoodLimit = max * 0.6;
-  const inputRef = React.createRef();
+	const [value, setValue] = useState(`${defaultValue}`);
+	const [mood, setMood] = useState(moods.NEUTRAL);
+	const [showTooltip, setShowtooltip] = useState(false);
+	const badMoodLimit = max * 0.4;
+	const gooMoodLimit = max * 0.6;
+	const inputRef = React.createRef();
 
-  useEffect(() => {
-    const valueN = parseFloat(value);
-    if (valueN === min) {
-      setMood(moods.BADBAD);
-    } else if (valueN === max) {
-      setMood(moods.BLISSFUL);
-    } else if (valueN < badMoodLimit) {
-      setMood(moods.BAD);
-    } else if (valueN > gooMoodLimit) {
-      setMood(moods.GOOD);
-    } else {
-      setMood(moods.NEUTRAL);
-    }
-    mkToolTip(valueN);
-    onValueChange(valueN);
-  }, [value]);
+	useEffect(() => {
+		const valueN = parseFloat(value);
+		if (valueN === min) {
+			setMood(moods.BADBAD);
+		} else if (valueN === max) {
+			setMood(moods.BLISSFUL);
+		} else if (valueN < badMoodLimit) {
+			setMood(moods.BAD);
+		} else if (valueN > gooMoodLimit) {
+			setMood(moods.GOOD);
+		} else {
+			setMood(moods.NEUTRAL);
+		}
+		onValueChange(valueN);
+		return doShowTooltip();
+	}, [value]);
 
-  useEffect(() => {
-    let timeTT;
-    if (tooltipPos > 0) {
-      timeTT && clearTimeout(timeTT);
-      setShowtooltip(true);
-      timeTT = setTimeout(() => {
-        setShowtooltip(false);
-      }, 2000);
-    }
-  }, [tooltipPos]);
+	const doShowTooltip = () => {
+		let timeTT;
+		timeTT && clearTimeout(timeTT);
+		setShowtooltip(true);
+		timeTT = setTimeout(() => {
+			setShowtooltip(false);
+		}, 3000);
+		return () => clearTimeout(timeTT);
+	};
 
-  const mkToolTip = valueN => {
-    const inputWidth = inputRef.current.offsetWidth;
-    const pos = ((inputWidth * (valueN - min)) / (max - min)) * 0.9;
-    setTooltipPos(pos);
-  };
+	const onChangeRange = e => {
+		const { value } = e.target;
+		setValue(value);
+	};
 
-  const onChangeRange = e => {
-    const { value } = e.target;
-    setValue(value);
-  };
+	const animateTooltip = show => ({
+		opacity: show ? 1 : 0,
+	});
 
-  const animateTooltip = (show, pos) => ({
-    opacity: show ? 1 : 0,
-    left: pos
-  });
+	return (
+		<div className="RangeSlider">
+			<div className="RangeSlider__side">{min}</div>
+			<div className="RangeSlider__center">
+				<div className="RangeSlider__tooltip" style={animateTooltip(showTooltip)}>
+					{labels.tooltip} {value}
+				</div>
 
-  return (
-    <div className="RangeSlider">
-      <div className="RangeSlider__side">{min}</div>
-      <div className="RangeSlider__center">
-        <div
-          className="RangeSlider__tooltip"
-          style={animateTooltip(showTooltip, tooltipPos)}
-        >
-          {value}
-        </div>
-
-        <input
-          className={`RangeSlider__input slider--${mood}`}
-          max={max}
-          min={min}
-          onChange={onChangeRange}
-          ref={inputRef}
-          step={step}
-          type="range"
-          value={value}
-        />
-      </div>
-      <div className="RangeSlider__side">{max}</div>
-    </div>
-  );
+				<input
+					className={`RangeSlider__input slider--${mood}`}
+					max={max}
+					min={min}
+					onChange={onChangeRange}
+					ref={inputRef}
+					step={step}
+					type="range"
+					value={value}
+				/>
+			</div>
+			<div className="RangeSlider__side">{max}</div>
+		</div>
+	);
 };
 
 export { RangeSlider };
